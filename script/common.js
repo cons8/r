1903e872462cf3a464b6c64bf5ad32eef757271c
const <ssk5k5@qq.com>
2025-08-23 11:51:42 +0800

    feat(timer):自定义标题

---
+++ script/common.js
@@ -0,0 +1,9 @@
+function showLog() {
+    document.querySelector('.mask').style.display = 'block';
+    document.querySelector('.log').style.display = 'flex';
+}
+
+function hideLog() {
+    document.querySelector('.mask').style.display = 'none';
+    document.querySelector('.log').style.display = 'none';
+}
